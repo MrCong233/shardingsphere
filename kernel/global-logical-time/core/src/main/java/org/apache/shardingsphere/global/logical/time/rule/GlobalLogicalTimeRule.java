d2dace4a2bf0a1cb215202d5b28448b9d92309af
congzhou2603 <zhoucong42@huawei.com>
2022-12-27 10:53:42 +0800

    kernel add global-logical-time model, a bug that can't load YamlRuleConfigurationSwapper.

---
+++ kernel/global-logical-time/core/src/main/java/org/apache/shardingsphere/global/logical/time/rule/GlobalLogicalTimeRule.java
@@ -0,0 +1,58 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.shardingsphere.global.logical.time.rule;
+
+import lombok.Getter;
+import org.apache.shardingsphere.global.logical.time.config.GlobalLogicalTimeRuleConfiguration;
+import org.apache.shardingsphere.global.logical.time.config.RedisConnectionOptionConfiguration;
+import org.apache.shardingsphere.infra.rule.identifier.scope.GlobalRule;
+
+/**
+ * global logical time rule.
+ */
+@Getter
+public final class GlobalLogicalTimeRule implements GlobalRule {
+    
+    private final GlobalLogicalTimeRuleConfiguration configuration;
+    
+    private final boolean globalLogicalTimeEnabled;
+    
+    private final RedisConnectionOptionConfiguration redisOption;
+    
+    public GlobalLogicalTimeRule(final GlobalLogicalTimeRuleConfiguration configuration) {
+        this.configuration = configuration;
+        this.globalLogicalTimeEnabled = configuration.isGlobalLogicalTimeEnabled();
+        this.redisOption = configuration.getRedisOption();
+    }
+    
+    @Override
+    public String getType() {
+        return GlobalLogicalTimeRule.class.getSimpleName();
+    }
+    
+    // TODO
+    
+    /**
+     * Get GLT engine.
+     *
+     * @param databaseType database type
+     */
+    public void getGlobalLogicalTimeEngine(final String databaseType) {
+        System.out.println("TODO");
+    }
+}
